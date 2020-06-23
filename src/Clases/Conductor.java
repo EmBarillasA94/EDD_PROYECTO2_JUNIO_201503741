e514b603546e7876d632f6cd7ca27c3470207c61
Eddy A. Murga Barillas <30765150+EmBarillasA94@users.noreply.github.com>
2020-06-22 22:52:24 -0600

    Creando la clase conductores

---
+++ src/Clases/Conductor.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author EDDY
+ */
+public class Conductor {
+
+    public Conductor(String dpi, String nombres, String apellidos, String tipo_licencia, String genero, int telefono, String direccion) {
+        this.dpi = dpi;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.tipo_licencia = tipo_licencia;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTipo_licencia() {
+        return tipo_licencia;
+    }
+
+    public void setTipo_licencia(String tipo_licencia) {
+        this.tipo_licencia = tipo_licencia;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    private String dpi;
+    private String nombres;
+    private String apellidos;
+    private String tipo_licencia;
+    private String genero;
+    private int telefono;
+    private String direccion;
+    
+}
