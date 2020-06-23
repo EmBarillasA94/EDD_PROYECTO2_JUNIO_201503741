e9afdb6a6d233cd2de520c6b552708fccfe6b191
Eddy A. Murga Barillas <30765150+EmBarillasA94@users.noreply.github.com>
2020-06-22 22:47:47 -0600

    Creando la clase vehiculo

---
+++ src/Clases/Vehiculo.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author EDDY
+ */
+public class Vehiculo {
+
+    public Vehiculo(String placa, String marca, String modelo, int anio, String color, int precio, String transmision) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.color = color;
+        this.precio = precio;
+        this.transmision = transmision;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+    
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int anio;
+    private String color;
+    private int precio;
+    private String transmision;
+}
