8251a9965619d856348f6ece30e675e93fac7ef3
Eddy A. Murga Barillas <30765150+EmBarillasA94@users.noreply.github.com>
2020-06-22 22:41:15 -0600

    Creando la clase cliente

---
+++ src/Clases/Cliente.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author EDDY
+ */
+public class Cliente {
+
+    public Cliente(int dpi, String nombres, String apellidos, String genero, int telefono, String direccion) {
+        this.dpi = dpi;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public int getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    private int dpi;
+    private String nombres;
+    private String apellidos;
+    private String genero;
+    private int telefono;
+    private String direccion;
+}
